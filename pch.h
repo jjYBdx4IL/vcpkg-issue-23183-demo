32a77cf59e93c0c2c1f09500d3acc6c0aa90b49a
jjYBdx4IL <jjYBdx4IL@github.com>
2022-02-21 14:56:50 +0100

    simplification

--- pch.h
+++ pch.h
@@ -3,16 +3,3 @@
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
-#include "wx/gbsizer.h"
-#include "wx/spinctrl.h"
-#include "wx/config.h"
-#include "wx/hyperlink.h"
-#include <wx/rawbmp.h>
-#include <wx/html/htmlwin.h>
-#include <wx/statline.h>
-#include <wx/sstream.h>
-#include <wx/taskbar.h>
-#include <wx/power.h>
-#include <wx/taskbarbutton.h>
-#include <wx/richtext/richtextctrl.h>
-
