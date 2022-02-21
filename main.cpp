753d4cac24ab0b7d2fc4b1efc92782e04cacb1ea
jjYBdx4IL <jjYBdx4IL@github.com>
2022-02-21 14:46:51 +0100

    123

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+wxIMPLEMENT_APP(MyApp);
+bool MyApp::OnInit()
+{
+    if (!wxApp::OnInit())
+        return false;
+    wxLogMessage("success");
+    return true;
+}
---
+++ pch.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+#include <wx/wx.h>
+#endif
+#include "wx/gbsizer.h"
+#include "wx/spinctrl.h"
+#include "wx/config.h"
+#include "wx/hyperlink.h"
+#include <wx/rawbmp.h>
+#include <wx/html/htmlwin.h>
+#include <wx/statline.h>
+#include <wx/sstream.h>
+#include <wx/taskbar.h>
+#include <wx/power.h>
+#include <wx/taskbarbutton.h>
+#include <wx/richtext/richtextctrl.h>
+
